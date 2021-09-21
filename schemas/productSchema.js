c761edc17f9082f0601b33987a27727e3027d2c4
bhomik33 <69043188+bhomik33@users.noreply.github.com>
2021-09-21 16:29:05 +1000

    Add files via upload

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+
+const productSchema = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    brand : {
+        type : String,
+        required : true
+    },
+    type : {
+        type: String,
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    }
+});
+
+module.exports = mongoose.model('Products', productSchema);
