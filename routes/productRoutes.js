59ba9c276cfba55865550d00b61cf7070ba34bba
bhomik33 <69043188+bhomik33@users.noreply.github.com>
2021-09-29 12:20:03 +1000

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,44 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../schemas/productSchema.js');
 
+// get request to show the add product form
 router.get('/', (req,res,next) => {
     const payload = null;
     res.render('product', {payload})
 })
 
+// get request to show the edit product form 
+router.get('/:id/edit', async(req,res,next) => {
+    const {id} = req.params;
+    const product =  await Product.findById(id);
+    res.render('editProduct', {product});
+})
+
+// update the products
+router.post('/:id', async(req,res,next) => {
+    const {id}  = req.params;
+    const name = req.body.productName;
+    const type = req.body.productType;
+    const brand = req.body.productBrand;
+    const quantity = req.body.productQuantity;
+
+    const product = await Product.findByIdAndUpdate(id,{name,type,brand,quantity});
+    await product.save();
+    res.redirect('/products');
+    
+})
+
+router.delete('/:id', async(req,res,next) => {
+    const {id} = req.params;
+    console.log(id);
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
+
+
+
+// post request to enter the product details to the database
 router.post('/', async (req,res,next) => {
     const name = req.body.productName;
     const type = req.body.productType;
@@ -22,7 +55,7 @@ router.post('/', async (req,res,next) => {
             brand,
             quantity
         })
-        res.redirect('/home');
+        res.redirect('/products');
     }
     else {
         payload.errorMessage = "Make sure each field has a valid value!";
